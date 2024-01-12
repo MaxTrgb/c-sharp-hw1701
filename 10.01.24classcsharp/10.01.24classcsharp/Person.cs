2ea1c451b26d9feeeda9ab0e2eeba5ad7c91d1d9
Max <126002400+MaxTrgb@users.noreply.github.com>
2024-01-12 16:27:23 +0200

    Add files via upload

---
+++ 10.01.24classcsharp/10.01.24classcsharp/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1001taskscsharp
+{
+    class Person
+    {
+        public String name { get; set; }
+        public int age { get; set; }
+        public Person(String name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
---
+++ 10.01.24classcsharp/10.01.24classcsharp/Program.cs
@@ -0,0 +1,56 @@
+﻿using _1001taskscsharp;
+using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace MySpace
+{
+    class Program
+    {
+        //public static void Main(string[] args)
+        //{
+        //    int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        //    Array.ForEach(nums, i =>
+        //    {
+        //        if (i % 2 == 0)
+        //        {
+        //            Console.WriteLine(i);
+        //        }
+        //    });
+        //}
+        //public static void Main(string[] args)
+        //{
+        //    int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //    int mid = 0;
+        //    Console.WriteLine(nums.Average());
+        //}
+        //public static void Main(string[] args)
+        //{
+        //    List<String> list = new List<String>();
+        //    list.Add("ab");
+        //    list.Add("acc");
+        //    list.Add("accc");
+        //    list.Add("avccccx");
+
+        //    Console.WriteLine(list.OrderDescending().First());
+        //}
+        public static void Main(string[] args)
+        {
+            List<Person> list = new List<Person>();
+
+            list.Add(new Person("Roman", 21));
+            list.Add(new Person("Anton", 31));
+            list.Add(new Person("Nazar", 8));
+            list.Add(new Person("Taras", 77));
+
+            list.ForEach(x =>
+            {
+                if (x.age >= 30)
+                {
+                    Console.WriteLine(x.name);
+                }
+            });
+
+        }
+
+    }
+}
